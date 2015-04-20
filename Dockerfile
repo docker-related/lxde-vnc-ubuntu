0173bab617b1c2db0a358daa0812eecc5399f58c
ekee si <553887976@qq.com>
2015-04-20 08:40:14 +0800

    First commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM ubuntu:trusty
+MAINTAINER ek <417@xmlad.com>
+
+ENV DEBIAN_FRONTEND noninteractive
+ENV HOME /root
+RUN apt-mark hold initscripts udev plymouth mountall
+RUN dpkg-divert --local --rename --add /sbin/initctl && ln -sf /bin/true /sbin/initctl
+
+RUN apt-get update \
+    && apt-get install -y --force-yes --no-install-recommends \
+        openssh-server sudo vim \
+        net-tools \
+        lxde x11vnc xvfb \
+        gtk2-engines-murrine ttf-ubuntu-font-family ttf-wqy-zenhei \
+        libreoffice firefox \
+		lxdm \
+    && apt-get autoclean \
+    && apt-get autoremove \
+    && rm -rf /var/lib/apt/lists/*
+RUN ln -s /usr/bin/python3 /usr/bin/python
+
+ADD noVNC /noVNC/
+ADD run.sh /
+EXPOSE 6080
+EXPOSE 6001
+EXPOSE 5900
+EXPOSE 22
+WORKDIR /
+ENTRYPOINT ["/run.sh"]
---
+++ run.sh
@@ -0,0 +1,64 @@
+#!/bin/bash
+stop_service()
+{
+# ps aux | grep "lxdm.*-d" | grep -v grep | awk {'print $2'} | while read i ; do kill -9 ${i}; done
+for i in lxsession x11vnc Xvfb; do pkill ${i}; done
+}
+initialize()
+{
+num=$(sed -n '/lxde-logout/=' /usr/share/lxde/openbox/menu.xml)
+for i in $(expr $num - 1) $num $(expr $num - 1)
+do
+sed -i "${i}d" /usr/share/lxde/openbox/menu.xml
+done
+echo "# ban" > /usr/bin/lxde-logout
+mkdir -p /var/run/sshd
+mknod -m 600 /dev/console c 5 1
+mknod /dev/tty0 c 4 0
+if
+[ -n "$USER_NAME" ]
+then
+result=0 && for name in $(cat /etc/passwd | cut -d ":" -f1)
+do
+[ "$USER_NAME" = "${name}" ] && result=$(expr $result + 1) && break
+done
+[ $result -ne 0 ] && USER_NAME=user
+else
+USER_NAME=user
+fi
+[ -n "$USER_PASSWORD" ] || USER_PASSWORD="pass"
+
+useradd --create-home --shell /bin/bash --user-group --groups adm,sudo $USER_NAME
+
+passwd $USER_NAME <<EOF >/dev/null 2>&1
+$USER_PASSWORD
+$USER_PASSWORD
+EOF
+
+passwd root <<EOF >/dev/null 2>&1
+root
+root
+EOF
+stop_service
+export DISPLAY=:1 
+export HOME="/home/$USER_NAME"
+echo "$USER_NAME@$USER_PASSWORD" > /home/$USER_NAME/.vncpass
+}
+username=$(ls /home/ | sed -n 1p)
+if
+[ -n "$username" ]
+then
+USER_NAME="$username"
+else
+initialize
+fi
+ps aux | grep -q lxdm || start-stop-daemon --start --quiet --pidfile /var/run/lxdm.pid --background --exec /usr/sbin/lxdm -- -d
+su $USER_NAME <<EOF
+export DISPLAY=:1 
+export HOME="/home/$USER_NAME"
+start-stop-daemon --start --quiet --pidfile /var/run/Xvfb.pid --background --exec /usr/bin/Xvfb -- :1 -screen 0 1024x640x16
+start-stop-daemon --start --quiet --pidfile /var/run/lxsession.pid --background --exec /usr/bin/lxsession -- -s LXDE -e LXDE
+start-stop-daemon --start --quiet --pidfile /var/run/x11vnc.pid --background --exec /usr/bin/x11vnc -- -xkb -forever -display :1 -passwdfile /home/$USER_NAME/.vncpass
+EOF
+[ -d /proc/$(cat /var/run/noVNC.pid) ] && start-stop-daemon --start --quiet --pidfile /var/run/noVNC.pid --background --exec /noVNC/utils/launch.sh
+exec /usr/sbin/sshd -D
